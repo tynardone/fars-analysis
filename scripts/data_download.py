48d5b8f0a16afbe5b1ed50fc1d13e4e5f2c18b9b
Tyler Nardone <tynardone@gmail.com>
2025-06-03 16:48:17 -0400

    Updated so that all file names are saved lower case

--- scripts/data_download.py
+++ scripts/data_download.py
@@ -5,6 +5,7 @@ import logging.config
 import sqlite3 as sq3
 import zipfile
 from pathlib import Path
+from typing import Iterable
 
 import httpx
 
@@ -19,16 +20,11 @@ logger = logging.getLogger(__name__)
 BASE_URL = "https://static.nhtsa.gov/nhtsa/downloads/FARS/{year}/National/FARS{year}NationalCSV.zip"
 DATA_DIR_ZIP = Path("fars_data_zip")
 DATA_DIR_UNZIP = Path("fars_data_unzipped")
-YEARS = range(2018, 2023)
+YEARS = range(2000, 2023)
 
 DATA_DIR_ZIP.mkdir(exist_ok=True)
 DATA_DIR_UNZIP.mkdir(exist_ok=True)
 
-# Fetch ZIP files using urls
-# Unzip files
-# Clean data
-# Figure out the database models
-# Save to SQLITE database
 
 # Connect to database
 sq3.connect("nhsta_fars.db")
@@ -54,24 +50,27 @@ def unzip_fars(zip_path: Path, dest_root: Path):
             Path(m).parts[0] == common_prefix for m in members
         )
 
-        if all_under_same_prefix:
-            for member in members:
-                member_path = Path(member)
-                parts = member_path.parts[1:]  # skip top-level folder
-                if parts:
-                    out_path = target_dir.joinpath(
-                        *parts
-                    )  # e.g. FARS2018National/accidents.csv
-                    out_path.parent.mkdir(parents=True, exist_ok=True)
+        for member in members:
+            member_path = Path(member)
+            parts: Iterable[str] = (
+                member_path.parts[1:] if all_under_same_prefix else member_path.parts
+            )
+            # Lower case all the file names and file extensions
+            parts = [p.lower() for p in parts]
 
-                    with zf.open(member) as source, open(out_path, "wb") as target:
-                        target.write(source.read())
+            if not parts:
+                continue  # skip if empty path
 
-        else:
-            zf.extractall(path=target_dir)
+            out_path = target_dir.joinpath(
+                *parts
+            )  # e.g. FARS2018National/accidents.csv
+            out_path.parent.mkdir(parents=True, exist_ok=True)
+
+            with zf.open(member) as source, open(out_path, "wb") as target:
+                target.write(source.read())
 
 
-async def download_zip(client: httpx.AsyncClient, year: int) -> None:
+async def download_zip(client: httpx.AsyncClient, year: int) -> bool:
     url = BASE_URL.format(year=year)
     file_destination = DATA_DIR_ZIP / f"FARS{year}National.zip"
 
@@ -81,15 +80,19 @@ async def download_zip(client: httpx.AsyncClient, year: int) -> None:
         response.raise_for_status()
         file_destination.write_bytes(response.content)
         logger.info(f"Downloaded {year} ZIP to {file_destination}")
+        return True
     except httpx.RequestError as exc:
         logger.exception(f"Request failed for {url}: {exc}")
     except httpx.HTTPStatusError as exc:
         logger.exception(f"Error response {exc.response.status_code} for {url}")
+    return False
 
 
 async def main():
     async with httpx.AsyncClient() as client:
-        await asyncio.gather(*(download_zip(client, year) for year in YEARS))
+        results = await asyncio.gather(*(download_zip(client, year) for year in YEARS))
+
+        successful_downloads = sum(results)
 
     for zip_file in DATA_DIR_ZIP.glob("*.zip"):
         # for each ZIP file in the zip directory, extract it and save files
@@ -97,6 +100,10 @@ async def main():
         unzip_fars(zip_file, DATA_DIR_UNZIP)
 
         logger.info(f"Unzipped {zip_file.name} to {DATA_DIR_UNZIP}")
+
+    logger.info(
+        f"Successfully downloaded {successful_downloads}/{len(YEARS)} ZIP files."
+    )
 
 
 if __name__ == "__main__":
